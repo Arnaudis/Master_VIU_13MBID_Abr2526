6b4343cf45463b93044447ea8fa44494aa3fe7c0
Arnaudis Suárez Sebastián <47118718+Arnaudis@users.noreply.github.com>
2025-10-28 17:20:38 +0000

    v2. Se instala la librería pytest-html para integrar una salida html a la ejecución de los tests básicos. Se actualiza el pipeline con estos casos. Además se incorporan las modifcaciones finales en figure.

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -8,6 +8,7 @@ import pytest
 
 # No es correcto leer para cada test el dataframe.
 # El decorador @pytest.fixture hace que se lea una sola vez y se pase a cada test. Pytest se encarga de gestionarlo.
+
 @pytest.fixture
 def datos_banco():
     """Carga los datos del banco desde el CSV
@@ -71,22 +72,22 @@ def test_basico(datos_banco):
     assert dataset.shape[1] == 21, f"El DataFrame debería tener 21 columnas, pero tiene {dataset.shape[1]}"
 
 
-     
+# Comentamos ya que no vamos a generar el archivo test_results.txt desde aquí.     
 # Para pasarlo a un pipeline, hay que convertirlo en un scrtip.py
-if __name__ == "__main__":
-    try:
-        # Ejecutar los tests directamente
-        test_esquema(datos_banco())
-        test_basico(datos_banco())
-        print("Todos los tests pasaron correctamente.")
-        with open("docs/test_results/test_results.txt", "w") as f:
-            # Grabamos fecha y hora
-            date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f"{date}")
-            f.write("Todos los tests pasaron correctamente.\n")
-    except AssertionError as e:
-        print(f"Fallo en los tests: {e}")
-        with open("docs/test_results/test_results.txt", "w") as f:
-            date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f"{date}")
-            f.write(f"Fallo en los tests: {e}\n")
+# if __name__ == "__main__":
+#     try:
+#         # Ejecutar los tests directamente
+#         test_esquema(datos_banco())
+#         test_basico(datos_banco())
+#         print("Todos los tests pasaron correctamente.")
+#         with open("docs/test_results/test_results.txt", "w") as f:
+#             # Grabamos fecha y hora
+#             date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#             f.write(f"{date}")
+#             f.write("Todos los tests pasaron correctamente.\n")
+#     except AssertionError as e:
+#         print(f"Fallo en los tests: {e}")
+#         with open("docs/test_results/test_results.txt", "w") as f:
+#             date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#             f.write(f"{date}")
+#             f.write(f"Fallo en los tests: {e}\n")
