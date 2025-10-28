e25fffeac51abf86480c364efea57859fc29d365
Arnaudis Suárez Sebastián <47118718+Arnaudis@users.noreply.github.com>
2025-10-28 15:06:18 +0000

    Inicio de la SP4. Generación de tests básicos con pytest.

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 # Define test orientados a la estructura del datraframe pues viene de pandas
 from pandera.pandas import DataFrameSchema, Column
@@ -9,6 +10,10 @@ import pytest
 # El decorador @pytest.fixture hace que se lea una sola vez y se pase a cada test. Pytest se encarga de gestionarlo.
 @pytest.fixture
 def datos_banco():
+    """Carga los datos del banco desde el CSV
+        Retorna:
+        pd.DataFrame: DataFrame con los datos del banco
+    """
     datos=pd.read_csv("data/raw/bank-additional-full.csv", sep=";")
     return datos
 
@@ -17,6 +22,9 @@ def datos_banco():
 # Se hacen varios tests pq un test da verdadero o falso y si juntamos varias validaciones en un solo test, 
 # puede que alguna falle y no sepamos cual.
 def test_esquema(datos_banco):
+    """Test para validar el esquema del dataframe del banco
+        Args: (pd.DataFrame): DataFrame con los datos del banco   
+    """
     dataset=datos_banco
     esquema = DataFrameSchema({
         "age": Column(int, nullable=False),
@@ -43,3 +51,42 @@ def test_esquema(datos_banco):
     })
     
     esquema.validate(dataset)
+
+
+
+def test_basico(datos_banco):
+    """Test básico para verificar que el datafram no esté vacío y tiene las columnas esperadas.
+        
+        Args: (pd.DataFrame): DataFrame con los datos del banco   
+    """
+    dataset=datos_banco
+    # Verificar que el DataFrame no esté vacío
+    assert not dataset.empty, "El DataFrame está vacío"
+    # Validar que no hay valores nulos
+    assert dataset.isnull().sum().sum() == 0, "El DataFrame contiene valores nulos"
+    # Vefiricar que tiene filas duplicadas
+    # Se ejecutará el siguiente assert tras las transformaciones que eliminen duplicados
+    # assert dataset.duplicated().sum() == 0, "El DataFrame contiene filas duplicadas"
+    # Validar que el número de columnas es el esperado
+    assert dataset.shape[1] == 21, f"El DataFrame debería tener 21 columnas, pero tiene {dataset.shape[1]}"
+
+
+     
+# Para pasarlo a un pipeline, hay que convertirlo en un scrtip.py
+if __name__ == "__main__":
+    try:
+        # Ejecutar los tests directamente
+        test_esquema(datos_banco())
+        test_basico(datos_banco())
+        print("Todos los tests pasaron correctamente.")
+        with open("docs/test_results/test_results.txt", "w") as f:
+            # Grabamos fecha y hora
+            date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{date}")
+            f.write("Todos los tests pasaron correctamente.\n")
+    except AssertionError as e:
+        print(f"Fallo en los tests: {e}")
+        with open("docs/test_results/test_results.txt", "w") as f:
+            date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{date}")
+            f.write(f"Fallo en los tests: {e}\n")
